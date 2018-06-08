40d0422182862291bc3fcb79bf8f1c0a42af1ee2
dlt-lee <664219378@qq.com>
2018-06-08 10:53:12 +0800

    add dlt.mda

---
+++ dlt.mda.R
@@ -0,0 +1,247 @@
+data<-dlt
+count<-dim(dlt)[1]
+data<-dlt
+count<-dim(dlt)[1]
+#dlt.mda <- function(data,count) {library(mda){
+  library(caret)
+  
+  
+  trains_1 <-tail(data,count)[1:(count-3),]
+  trains_2 <-tail(data,count)[2:(count-2),]
+  trains_3 <-tail(data,count)[3:(count-1),]
+  results<-tail(data,(count-3))
+  tests_1<-tail(data,count)[1:(count-2),]
+  tests_2<-tail(data,count)[2:(count-1),]
+  tests_3<-tail(data,(count-2))
+  
+  #A:
+  trn1<-trains_1$n
+  trn2<-trains_2$n
+  trn3<-trains_3$n
+  a1.1<-trains_1$a1
+  a2.1<-trains_1$a2
+  a3.1<-trains_1$a3
+  a4.1<-trains_1$a4
+  a5.1<-trains_1$a5
+  a1.2<-trains_2$a1
+  a2.2<-trains_2$a2
+  a3.2<-trains_2$a3
+  a4.2<-trains_2$a4
+  a5.2<-trains_2$a5
+  a1.3<-trains_3$a1
+  a2.3<-trains_3$a2
+  a3.3<-trains_3$a3
+  a4.3<-trains_3$a4
+  a5.3<-trains_3$a5
+  resa1<-results$a1
+  resa2<-results$a2
+  resa3<-results$a3
+  resa4<-results$a4
+  resa5<-results$a5
+  #B:
+  b1.1<-trains_1$b1
+  b2.1<-trains_1$b2
+  b1.2<-trains_2$b1
+  b2.2<-trains_2$b2
+  b1.3<-trains_3$b1
+  b2.3<-trains_3$b2
+  resb1<-results$b1
+  resb2<-results$b2
+  
+  trains.a1<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa1)
+  trains.a2<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa2)
+  trains.a3<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa3)
+  trains.a4<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa4)
+  trains.a5<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa5)
+  trains.b1<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resb1)
+  trains.b2<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resb2)
+  
+  set.seed(100)
+  
+  mdaModel.a1<-mda(resa1~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.a1,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 22
+  )
+  mdaModel.a2<-mda(resa2~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.a2,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 19
+  )
+  mdaModel.a3<-mda(resa3~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.a3,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 29
+  )
+  mdaModel.a4<-mda(resa4~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.a4,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 29
+  )
+  mdaModel.a5<-mda(resa5~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.a5,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 21
+  )
+  mdaModel.b1<-mda(resb1~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.b1,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 9
+  )
+  mdaModel.b2<-mda(resb2~
+                     #a1.1+a2.1+a3.1+a4.1+a5.1+
+                     #a1.2+a2.2+a3.2+a4.2+a5.2+
+                     a1.3+a2.3+a3.3+a4.3+a5.3+
+                     #b1.1+b2.1+
+                     #b1.2+b2.2+
+                     b1.3+b2.3,
+                   data = trains.b2,
+                   preProcess = c("BoxCox","center","scale","pca"),
+                   #range(dlt$a1)-1
+                   #subclasses = 9
+  )
+  
+  
+  #Buil test data
+  #A:
+  tsn1<-tests_1$n
+  tsn2<-tests_2$n
+  tsn3<-tests_3$n
+  a1.1<-tests_1$a1
+  a2.1<-tests_1$a2
+  a3.1<-tests_1$a3
+  a4.1<-tests_1$a4
+  a5.1<-tests_1$a5
+  a1.2<-tests_2$a1
+  a2.2<-tests_2$a2
+  a3.2<-tests_2$a3
+  a4.2<-tests_2$a4
+  a5.2<-tests_2$a5
+  a1.3<-tests_3$a1
+  a2.3<-tests_3$a2
+  a3.3<-tests_3$a3
+  a4.3<-tests_3$a4
+  a5.3<-tests_3$a5
+  #B:
+  b1.1<-tests_1$b1
+  b2.1<-tests_1$a2
+  b1.2<-tests_2$b1
+  b2.2<-tests_2$b2
+  b1.3<-tests_3$b1
+  b2.3<-tests_3$b2
+  
+  tests.ab<-data.frame(tsn1,tsn2,tsn3,
+                       a1.1,a2.1,a3.1,a4.1,a5.1,
+                       a1.2,a2.2,a3.2,a4.2,a5.2,
+                       a1.3,a2.3,a3.3,a4.3,a5.3,
+                       b1.1,b2.1,
+                       b2.2,b2.2,
+                       b2.3,b2.3)
+  test.ab<-preProcess(tests.ab,method = c("BoxCox","center","scale","pca"))
+  testPredictions.a1<-predict(mdaModel.a1,tests.ab)
+  testPredictions.a2<-predict(mdaModel.a2,tests.ab)
+  testPredictions.a3<-predict(mdaModel.a3,tests.ab)
+  testPredictions.a4<-predict(mdaModel.a4,tests.ab)
+  testPredictions.a5<-predict(mdaModel.a5,tests.ab)
+  testPredictions.b1<-predict(mdaModel.b1,tests.ab)
+  testPredictions.b2<-predict(mdaModel.b2,tests.ab)
+  
+  print(c(tail(testPredictions.a1,1),
+          tail(testPredictions.a2,1),
+          tail(testPredictions.a3,1),  
+          tail(testPredictions.a4,1),
+          tail(testPredictions.a5,1),
+          tail(testPredictions.b1,1),
+          tail(testPredictions.b2,1)))
+#}
