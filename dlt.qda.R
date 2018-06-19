55d9810aea937c93fee8e251f0b0d6fca41a2c6d
dlt-lee <664219378@qq.com>
2018-06-19 15:07:45 +0800

    add file

---
+++ dlt.qda.R
@@ -0,0 +1,106 @@
+data<-dlt
+count<-dim(dlt)[1]
+dlt.qda <- function(data,count) {
+  library(MASS)
+  
+  trains_1 <-tail(data,count)[1:(count-3),]
+  trains_2 <-tail(data,count)[2:(count-2),]
+  trains_3 <-tail(data,count)[3:(count-1),]
+  results<-tail(data,(count-3))
+  tests_1<-tail(data,count)[1:(count-2),]
+  tests_2<-tail(data,count)[2:(count-1),]
+  tests_3<-tail(data,(count-2))
+  
+  #A:
+  trn1<-trains_1$n
+  trn2<-trains_2$n
+  trn3<-trains_3$n
+  a1.1<-trains_1$a1
+  a2.1<-trains_1$a2
+  a3.1<-trains_1$a3
+  a4.1<-trains_1$a4
+  a5.1<-trains_1$a5
+  a1.2<-trains_2$a1
+  a2.2<-trains_2$a2
+  a3.2<-trains_2$a3
+  a4.2<-trains_2$a4
+  a5.2<-trains_2$a5
+  a1.3<-trains_3$a1
+  a2.3<-trains_3$a2
+  a3.3<-trains_3$a3
+  a4.3<-trains_3$a4
+  a5.3<-trains_3$a5
+  resa1<-dlt.d2c(results$a1)
+  resa2<-dlt.d2c(results$a2)
+  resa3<-dlt.d2c(results$a3)
+  resa4<-dlt.d2c(results$a4)
+  resa5<-dlt.d2c(results$a5)
+  #B:
+  b1.1<-trains_1$b1
+  b2.1<-trains_1$b2
+  b1.2<-trains_2$b1
+  b2.2<-trains_2$b2
+  b1.3<-trains_3$b1
+  b2.3<-trains_3$b2
+  resb1<-dlt.d2c(results$b1)
+  resb2<-dlt.d2c(results$b2)
+  
+  trains.a1<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa1)
+  trains.a2<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa2)
+  trains.a3<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa3)
+  trains.a4<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa4)
+  trains.a5<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resa5)
+  trains.b1<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resb1)
+  trains.b2<-data.frame(trn1,trn2,trn3,
+                        a1.1,a2.1,a3.1,a4.1,a5.1,
+                        a1.2,a2.2,a3.2,a4.2,a5.2,
+                        a1.3,a2.3,a3.3,a4.3,a5.3,
+                        b1.1,b2.1,
+                        b1.2,b2.2,
+                        b1.3,b2.3,
+                        resb2)
+  
+  
+}
