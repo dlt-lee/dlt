c0daa2ff0b25995d94f25bfe3aaf0697cc590c4a
dlt-lee <664219378@qq.com>
2018-06-28 13:26:02 +0800

    modify

--- for.function.R
+++ for.function.R
@@ -2,7 +2,7 @@ data<-dlt
 count<-dim(dlt)[1]
 temp.ab<-c(0,0,0,0,0,0,0,0)
 seed<-c(0,1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97)
-for (i in 1:30) {
+for (i in seed) {
     temp.c<-dlt.pr.xgb.XX(data,count,i)
   temp.ab<-c(temp.ab,i,temp.c)
 }
