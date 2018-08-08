da930b2282154ebc05975b00fd70ab75e8626702
dlt-lee <664219378@qq.com>
2018-08-08 12:41:13 +0800

    add data

--- dlt.class.rf.R
+++ dlt.class.rf.R
@@ -104,70 +104,70 @@ count<-dim(dlt)[1]
                         resb2)
   
   rf.model.a1<-randomForest(resa1~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
                               a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
                               b1.1+b2.1+
-                              #b1.2+b2.2+
+                              b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.a1,
                             ntree = 2000)
   rf.model.a2<-randomForest(resa2~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
                               a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
+                              b1.1+b2.1+
                               b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.a2,
                             ntree = 2000)
   
   rf.model.a3<-randomForest(resa3~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
                               a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
+                              b1.1+b2.1+
                               b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.a3,
                             ntree = 2000)
   
   rf.model.a4<-randomForest(resa4~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
                               a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
+                              b1.1+b2.1+
                               b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.a4,
                             ntree = 2000)
   
   rf.model.a5<-randomForest(resa5~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
                               a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
+                              b1.1+b2.1+
                               b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.a5,
                             ntree = 2000)
   
   rf.model.b1<-randomForest(resb1~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
-                              #a1.2+a2.2+a3.2+a4.2+a5.2+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
-                              #b1.2+b2.2+
+                              b1.1+b2.1+
+                              b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.b1,
                             ntree = 2000)
   
   rf.model.b2<-randomForest(resb2~
-                              #a1.1+a2.1+a3.1+a4.1+a5.1+
-                              #a1.2+a2.2+a3.2+a4.2+a5.2+
+                              a1.1+a2.1+a3.1+a4.1+a5.1+
+                              a1.2+a2.2+a3.2+a4.2+a5.2+
                               a1.3+a2.3+a3.3+a4.3+a5.3+
-                              #b1.1+b2.1+
-                              #b1.2+b2.2+
+                              b1.1+b2.1+
+                              b1.2+b2.2+
                               b1.3+b2.3,
                             data = trains.b2,
                             ntree = 2000)
@@ -217,38 +217,38 @@ count<-dim(dlt)[1]
   rfTestPred.b1<-predict(rf.model.b1,tests.ab,type = "prob")
   rfTestPred.b2<-predict(rf.model.b2,tests.ab,type = "prob")
   
-  tests.ab$RFprob.a101<-rfTestPred.a1[,"01"]
-  tests.ab$RFprob.a102<-rfTestPred.a1[,"02"]
-  tests.ab$RFprob.a103<-rfTestPred.a1[,"03"]
-  tests.ab$RFprob.a104<-rfTestPred.a1[,"04"]
-  tests.ab$RFprob.a105<-rfTestPred.a1[,"05"]
-  tests.ab$RFprob.a106<-rfTestPred.a1[,"06"]
-  tests.ab$RFprob.a107<-rfTestPred.a1[,"07"]
-  tests.ab$RFprob.a108<-rfTestPred.a1[,"08"]
-  tests.ab$RFprob.a109<-rfTestPred.a1[,"09"]
-  tests.ab$RFprob.a110<-rfTestPred.a1[,"10"]
-  tests.ab$RFprob.a111<-rfTestPred.a1[,"11"]
-  tests.ab$RFprob.a112<-rfTestPred.a1[,"12"]
-  tests.ab$RFprob.a113<-rfTestPred.a1[,"13"]
-  tests.ab$RFprob.a114<-rfTestPred.a1[,"14"]
-  tests.ab$RFprob.a115<-rfTestPred.a1[,"15"]
-  tests.ab$RFprob.a116<-rfTestPred.a1[,"16"]
-  tests.ab$RFprob.a117<-rfTestPred.a1[,"17"]
-  tests.ab$RFprob.a118<-rfTestPred.a1[,"18"]
-  tests.ab$RFprob.a119<-rfTestPred.a1[,"19"]
-  tests.ab$RFprob.a120<-rfTestPred.a1[,"20"]
-  tests.ab$RFprob.a121<-rfTestPred.a1[,"21"]
-  tests.ab$RFprob.a122<-rfTestPred.a1[,"22"]
-  tests.ab$RFprob.a123<-rfTestPred.a1[,"23"]
-  tests.ab$RFprob.a124<-rfTestPred.a1[,"24"]
+  #tests.ab$RFprob.a101<-rfTestPred.a1[,"01"]
+  #tests.ab$RFprob.a102<-rfTestPred.a1[,"02"]
+  #tests.ab$RFprob.a103<-rfTestPred.a1[,"03"]
+  #tests.ab$RFprob.a104<-rfTestPred.a1[,"04"]
+  #tests.ab$RFprob.a105<-rfTestPred.a1[,"05"]
+  #tests.ab$RFprob.a106<-rfTestPred.a1[,"06"]
+  #tests.ab$RFprob.a107<-rfTestPred.a1[,"07"]
+  #tests.ab$RFprob.a108<-rfTestPred.a1[,"08"]
+  #tests.ab$RFprob.a109<-rfTestPred.a1[,"09"]
+  #tests.ab$RFprob.a110<-rfTestPred.a1[,"10"]
+  #tests.ab$RFprob.a111<-rfTestPred.a1[,"11"]
+  #tests.ab$RFprob.a112<-rfTestPred.a1[,"12"]
+  #tests.ab$RFprob.a113<-rfTestPred.a1[,"13"]
+  #tests.ab$RFprob.a114<-rfTestPred.a1[,"14"]
+  #tests.ab$RFprob.a115<-rfTestPred.a1[,"15"]
+  #tests.ab$RFprob.a116<-rfTestPred.a1[,"16"]
+  #tests.ab$RFprob.a117<-rfTestPred.a1[,"17"]
+  #tests.ab$RFprob.a118<-rfTestPred.a1[,"18"]
+  #tests.ab$RFprob.a119<-rfTestPred.a1[,"19"]
+  #tests.ab$RFprob.a120<-rfTestPred.a1[,"20"]
+  #tests.ab$RFprob.a121<-rfTestPred.a1[,"21"]
+  #tests.ab$RFprob.a122<-rfTestPred.a1[,"22"]
+  #tests.ab$RFprob.a123<-rfTestPred.a1[,"23"]
+  #tests.ab$RFprob.a124<-rfTestPred.a1[,"24"]
   
-  tests.ab$RFclass.a1<-predict(rf.model.a1,tests.ab)
-  tests.ab$RFclass.a2<-predict(rf.model.a2,tests.ab)
-  tests.ab$RFclass.a3<-predict(rf.model.a3,tests.ab)
-  tests.ab$RFclass.a4<-predict(rf.model.a4,tests.ab)
-  tests.ab$RFclass.a5<-predict(rf.model.a5,tests.ab)
-  tests.ab$RFclass.b1<-predict(rf.model.b1,tests.ab)
-  tests.ab$RFclass.b2<-predict(rf.model.b2,tests.ab)
+  #tests.ab$RFclass.a1<-predict(rf.model.a1,tests.ab)
+  #tests.ab$RFclass.a2<-predict(rf.model.a2,tests.ab)
+  #tests.ab$RFclass.a3<-predict(rf.model.a3,tests.ab)
+  #tests.ab$RFclass.a4<-predict(rf.model.a4,tests.ab)
+  #tests.ab$RFclass.a5<-predict(rf.model.a5,tests.ab)
+  #tests.ab$RFclass.b1<-predict(rf.model.b1,tests.ab)
+  #tests.ab$RFclass.b2<-predict(rf.model.b2,tests.ab)
   
   barplot(tail(rfTestPred.a1,1),main = "a1")
   barplot(tail(rfTestPred.a2,1),main = "a2")
@@ -258,6 +258,52 @@ count<-dim(dlt)[1]
   barplot(tail(rfTestPred.b1,1),main = "b1")
   barplot(tail(rfTestPred.b2,1),main = "b2")
   
+  dn.a1<-names(tail(rfTestPred.a1,1)[1,])
+  pn.a1<-tail(rfTestPred.a1,1)[1,]
+  r.a1<-data.frame(dn.a1,pn.a1)
+  
+  dn.a2<-names(tail(rfTestPred.a2,1)[1,])
+  pn.a2<-tail(rfTestPred.a2,1)[1,]
+  r.a2<-data.frame(dn.a2,pn.a2)
+  
+  dn.a3<-names(tail(rfTestPred.a3,1)[1,])
+  pn.a3<-tail(rfTestPred.a3,1)[1,]
+  r.a3<-data.frame(dn.a3,pn.a3)
+  
+  dn.a4<-names(tail(rfTestPred.a4,1)[1,])
+  pn.a4<-tail(rfTestPred.a4,1)[1,]
+  r.a4<-data.frame(dn.a4,pn.a4)
+  
+  dn.a5<-names(tail(rfTestPred.a5,1)[1,])
+  pn.a5<-tail(rfTestPred.a5,1)[1,]
+  r.a5<-data.frame(dn.a5,pn.a5)
+  
+  dn.b1<-names(tail(rfTestPred.b1,1)[1,])
+  pn.b1<-tail(rfTestPred.b1,1)[1,]
+  r.b1<-data.frame(dn.b1,pn.b1)
+  
+  dn.b2<-names(tail(rfTestPred.b2,1)[1,])
+  pn.b2<-tail(rfTestPred.b2,1)[1,]
+  r.b2<-data.frame(dn.b2,pn.b2)
+  
+  c(
+    r.a1[which(dn.a1=="26"),2],
+    r.a2[which(dn.a2=="26"),2],
+    r.a3[which(dn.a3=="26"),2],
+    r.a4[which(dn.a4=="26"),2],
+    r.a5[which(dn.a5=="26"),2],
+    r.b1[which(dn.b1=="26"),2],
+    r.b2[which(dn.b2=="26"),2]
+  )
+  
+  
+  tail(rfTestPred.a1,1)
+  tail(rfTestPred.a2,1)
+  tail(rfTestPred.a3,1)
+  tail(rfTestPred.a4,1)
+  tail(rfTestPred.a5,1)
+  tail(rfTestPred.b1,1)
+  tail(rfTestPred.b2,1)
 #}
 
 
