1e03d338bb49de5a9d4c61a66ac855e89b527f74
dlt-lee <664219378@qq.com>
2018-09-06 11:12:31 +0800

    modify data

--- for.clu.xgb.R
+++ for.clu.xgb.R
@@ -10,6 +10,14 @@ r5<-dlt.clu.xgb.XV(data,count,n)
 r6<-dlt.clu.xgb.XVIII(data,count,n)
 r7<-dlt.clu.xgb.XXI(data,count,n)
 
+table(c(r1[1],r2[1],r3[1],r4[1],r5[1],r6[1],r7[1]))
+table(c(r1[2],r2[2],r3[2],r4[2],r5[2],r6[2],r7[2]))
+table(c(r1[3],r2[3],r3[3],r4[3],r5[3],r6[3],r7[3]))
+table(c(r1[4],r2[4],r3[4],r4[4],r5[4],r6[4],r7[4]))
+table(c(r1[5],r2[5],r3[5],r4[5],r5[5],r6[5],r7[5]))
+table(c(r1[6],r2[6],r3[6],r4[6],r5[6],r6[6],r7[6]))
+table(c(r1[7],r2[7],r3[7],r4[7],r5[7],r6[7],r7[7]))
+
 
 r1
 r2
