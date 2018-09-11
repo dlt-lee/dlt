6f87333dee727244c9c8ac69ddafb65e5bb96908
dlt-lee <664219378@qq.com>
2018-09-11 16:44:54 +0800

    modify code

--- dlt.clu.xgb.XXIV.R
+++ dlt.clu.xgb.XXIV.R
@@ -536,7 +536,7 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                         b1.7,b1.8,a1.9,
                         b1.10,b1.11,b1.12,
                         b1.13,b1.14,b1.15,
-                        b1.16,n1.17,b1.18,
+                        b1.16,b1.17,b1.18,
                         b1.19,b1.20,b1.21,
                         b1.22,b1.23,b1.24,
                         b1.25,b1.26,b1.27,
@@ -718,6 +718,126 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
   a4.22<-tests_22$a4
   a5.22<-tests_22$a5
   
+  a1.23<-tests_23$a1
+  a2.23<-tests_23$a2
+  a3.23<-tests_23$a3
+  a4.23<-tests_23$a4
+  a5.23<-tests_23$a5
+  
+  a1.24<-tests_24$a1
+  a2.24<-tests_24$a2
+  a3.24<-tests_24$a3
+  a4.24<-tests_24$a4
+  a5.24<-tests_24$a5
+  
+  a1.25<-tests_25$a1
+  a2.25<-tests_25$a2
+  a3.25<-tests_25$a3
+  a4.25<-tests_25$a4
+  a5.25<-tests_25$a5
+  
+  a1.26<-tests_26$a1
+  a2.26<-tests_26$a2
+  a3.26<-tests_26$a3
+  a4.26<-tests_26$a4
+  a5.26<-tests_26$a5
+  
+  a1.27<-tests_27$a1
+  a2.27<-tests_27$a2
+  a3.27<-tests_27$a3
+  a4.27<-tests_27$a4
+  a5.27<-tests_27$a5
+  
+  a1.28<-tests_28$a1
+  a2.28<-tests_28$a2
+  a3.28<-tests_28$a3
+  a4.28<-tests_28$a4
+  a5.28<-tests_28$a5
+  
+  a1.29<-tests_29$a1
+  a2.29<-tests_29$a2
+  a3.29<-tests_29$a3
+  a4.29<-tests_29$a4
+  a5.29<-tests_29$a5
+  
+  a1.30<-tests_30$a1
+  a2.30<-tests_30$a2
+  a3.30<-tests_30$a3
+  a4.30<-tests_30$a4
+  a5.30<-tests_30$a5
+  
+  a1.31<-tests_31$a1
+  a2.31<-tests_31$a2
+  a3.31<-tests_31$a3
+  a4.31<-tests_31$a4
+  a5.31<-tests_31$a5
+  
+  a1.32<-tests_32$a1
+  a2.32<-tests_32$a2
+  a3.32<-tests_32$a3
+  a4.32<-tests_32$a4
+  a5.32<-tests_32$a5
+  
+  a1.33<-tests_33$a1
+  a2.33<-tests_33$a2
+  a3.33<-tests_33$a3
+  a4.33<-tests_33$a4
+  a5.33<-tests_33$a5
+  
+  a1.34<-tests_34$a1
+  a2.34<-tests_34$a2
+  a3.34<-tests_34$a3
+  a4.34<-tests_34$a4
+  a5.34<-tests_34$a5
+  
+  a1.35<-tests_35$a1
+  a2.35<-tests_35$a2
+  a3.35<-tests_35$a3
+  a4.35<-tests_35$a4
+  a5.35<-tests_35$a5
+  
+  a1.36<-tests_36$a1
+  a2.36<-tests_36$a2
+  a3.36<-tests_36$a3
+  a4.36<-tests_36$a4
+  a5.36<-tests_36$a5
+  
+  a1.37<-tests_37$a1
+  a2.37<-tests_37$a2
+  a3.37<-tests_37$a3
+  a4.37<-tests_37$a4
+  a5.37<-tests_37$a5
+  
+  a1.38<-tests_38$a1
+  a2.38<-tests_38$a2
+  a3.38<-tests_38$a3
+  a4.38<-tests_38$a4
+  a5.38<-tests_38$a5
+  
+  a1.39<-tests_39$a1
+  a2.39<-tests_39$a2
+  a3.39<-tests_39$a3
+  a4.39<-tests_39$a4
+  a5.39<-tests_39$a5
+  
+  a1.40<-tests_40$a1
+  a2.40<-tests_40$a2
+  a3.40<-tests_40$a3
+  a4.40<-tests_40$a4
+  a5.40<-tests_40$a5
+  
+  a1.41<-tests_41$a1
+  a2.41<-tests_41$a2
+  a3.41<-tests_41$a3
+  a4.41<-tests_41$a4
+  a5.41<-tests_41$a5
+  
+  a1.42<-tests_42$a1
+  a2.42<-tests_42$a2
+  a3.42<-tests_42$a3
+  a4.42<-tests_42$a4
+  a5.42<-tests_42$a5
+  
   #B:
   b1.1<-tests_1$b1
   b2.1<-tests_1$b2
@@ -737,9 +857,9 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
   b2.8<-tests_8$b2
   b1.9<-tests_9$b1
   b2.9<-tests_9$b2
+  
   b1.10<-tests_10$b1
   b2.10<-tests_10$b2
-  
   b1.11<-tests_11$b1
   b2.11<-tests_11$b2
   b1.12<-tests_12$b1
@@ -758,10 +878,55 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
   b2.18<-tests_18$b2
   b1.19<-tests_19$b1
   b2.19<-tests_19$b2
+  
   b1.20<-tests_20$b1
   b2.20<-tests_20$b2
   b1.21<-tests_21$b1
   b2.21<-tests_21$b2
+  b1.22<-tests_22$b1
+  b2.22<-tests_22$b2
+  b1.23<-tests_23$b1
+  b2.23<-tests_23$b2
+  b1.24<-tests_24$b1
+  b2.24<-tests_24$b2
+  b1.25<-tests_25$b1
+  b2.25<-tests_25$b2
+  b1.26<-tests_26$b1
+  b2.26<-tests_26$b2
+  b1.27<-tests_27$b1
+  b2.27<-tests_27$b2
+  b1.28<-tests_28$b1
+  b2.28<-tests_28$b2
+  b1.29<-tests_29$b1
+  b2.29<-tests_29$b2
+  
+  b1.30<-tests_30$b1
+  b2.30<-tests_30$b2
+  b1.31<-tests_31$b1
+  b2.31<-tests_31$b2
+  b1.32<-tests_32$b1
+  b2.32<-tests_32$b2
+  b1.33<-tests_33$b1
+  b2.33<-tests_33$b2
+  b1.34<-tests_34$b1
+  b2.34<-tests_34$b2
+  b1.35<-tests_35$b1
+  b2.35<-tests_35$b2
+  b1.36<-tests_36$b1
+  b2.36<-tests_36$b2
+  b1.37<-tests_37$b1
+  b2.37<-tests_37$b2
+  b1.38<-tests_38$b1
+  b2.38<-tests_38$b2
+  b1.39<-tests_39$b1
+  b2.39<-tests_39$b2
+  
+  b1.40<-tests_40$b1
+  b2.40<-tests_40$b2
+  b1.41<-tests_41$b1
+  b2.41<-tests_41$b2
+  b1.42<-tests_42$b1
+  b2.42<-tests_42$b2
   
   tests.a1<-data.frame(tsn1,tsn2,tsn3,
                        a1.1,a1.2,a1.3,
@@ -770,7 +935,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        a1.10,a1.11,a1.12,
                        a1.13,a1.14,a1.15,
                        a1.16,a1.17,a1.18,
-                       a1.19,a1.20,a1.21
+                       a1.19,a1.20,a1.21,
+                       a1.22,a1.23,a1.24,
+                       a1.25,a1.26,a1.27,
+                       a1.28,a1.29,a1.30,
+                       a1.31,a1.32,a1.33,
+                       a1.34,a1.35,a1.36,
+                       a1.37,a1.38,a1.39,
+                       a1.40,a1.41,a1.42
   )
   tests.a2<-data.frame(tsn1,tsn2,tsn3,
                        a2.1,a2.2,a2.3,
@@ -779,7 +951,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        a2.10,a2.11,a2.12,
                        a2.13,a2.14,a2.15,
                        a2.16,a2.17,a2.18,
-                       a2.19,a2.20,a2.21
+                       a2.19,a2.20,a2.21,
+                       a2.22,a2.23,a2.24,
+                       a2.25,a2.26,a2.27,
+                       a2.28,a2.29,a2.30,
+                       a2.31,a2.32,a2.33,
+                       a2.34,a2.35,a2.36,
+                       a2.37,a2.38,a2.39,
+                       a2.40,a2.41,a2.42
   )
   tests.a3<-data.frame(tsn1,tsn2,tsn3,
                        a3.1,a3.2,a3.3,
@@ -788,7 +967,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        a3.10,a3.11,a3.12,
                        a3.13,a3.14,a3.15,
                        a3.16,a3.17,a3.18,
-                       a3.19,a3.20,a3.21
+                       a3.19,a3.20,a3.21,
+                       a3.22,a3.23,a3.24,
+                       a3.25,a3.26,a3.27,
+                       a3.28,a3.29,a3.30,
+                       a3.31,a3.32,a3.33,
+                       a3.34,a3.35,a3.36,
+                       a3.37,a3.38,a3.39,
+                       a3.40,a3.41,a3.42
   )
   tests.a4<-data.frame(tsn1,tsn2,tsn3,
                        a4.1,a4.2,a4.3,
@@ -797,7 +983,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        a4.10,a4.11,a4.12,
                        a4.13,a4.14,a4.15,
                        a4.16,a4.17,a4.18,
-                       a4.19,a4.20,a4.21
+                       a4.19,a4.20,a4.21,
+                       a4.22,a4.23,a4.24,
+                       a4.25,a4.26,a4.27,
+                       a4.28,a4.29,a4.30,
+                       a4.31,a4.32,a4.33,
+                       a4.34,a4.35,a4.36,
+                       a4.37,a4.38,a4.39,
+                       a4.40,a4.41,a4.42
   )
   tests.a5<-data.frame(tsn1,tsn2,tsn3,
                        a5.1,a5.2,a5.3,
@@ -806,7 +999,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        a5.10,a5.11,a5.12,
                        a5.13,a5.14,a5.15,
                        a5.16,a5.17,a5.18,
-                       a5.19,a5.20,a5.21
+                       a5.19,a5.20,a5.21,
+                       a5.22,a5.23,a5.24,
+                       a5.25,a5.26,a5.27,
+                       a5.28,a5.29,a5.30,
+                       a5.31,a5.32,a5.33,
+                       a5.34,a5.35,a5.36,
+                       a5.37,a5.38,a5.39,
+                       a5.40,a5.41,a5.42
   )
   tests.b1<-data.frame(tsn1,tsn2,tsn3,
                        b1.1,b1.2,b1.3,
@@ -815,7 +1015,14 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        b1.10,b1.11,b1.12,
                        b1.13,b1.14,b1.15,
                        b1.16,b1.17,b1.18,
-                       b1.19,b1.20,b1.21
+                       b1.19,b1.20,b1.21,
+                       b1.22,b1.23,b1.24,
+                       b1.25,b1.26,b1.27,
+                       b1.28,b1.29,b1.30,
+                       b1.31,b1.32,b1.33,
+                       b1.34,b1.35,b1.36,
+                       b1.37,b1.38,b1.39,
+                       a1.40,b1.41,b1.42
   )
   tests.b2<-data.frame(tsn1,tsn2,tsn3,
                        b2.1,b2.2,b2.3,
@@ -824,16 +1031,23 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
                        b2.10,b2.11,b2.12,
                        b2.13,b2.14,b2.15,
                        b2.16,b2.17,b2.18,
-                       b2.19,b2.20,b2.21
+                       b2.19,b2.20,b2.21,
+                       b2.22,b2.23,b2.24,
+                       b2.25,b2.26,b2.27,
+                       b2.28,b2.29,b2.30,
+                       b2.31,b2.32,b2.33,
+                       b2.34,b2.35,b2.36,
+                       b2.37,b2.38,b2.39,
+                       b2.40,b2.41,b2.42
   )
   
-  tests.T.a1<-Matrix(as.matrix(tests.a1[,4:24]),sparse=T)
-  tests.T.a2<-Matrix(as.matrix(tests.a2[,4:24]),sparse=T)
-  tests.T.a3<-Matrix(as.matrix(tests.a3[,4:24]),sparse=T)
-  tests.T.a4<-Matrix(as.matrix(tests.a4[,4:24]),sparse=T)
-  tests.T.a5<-Matrix(as.matrix(tests.a5[,4:24]),sparse=T)
-  tests.T.b1<-Matrix(as.matrix(tests.b1[,4:24]),sparse=T)
-  tests.T.b2<-Matrix(as.matrix(tests.b2[,4:24]),sparse=T)
+  tests.T.a1<-Matrix(as.matrix(tests.a1[,4:45]),sparse=T)
+  tests.T.a2<-Matrix(as.matrix(tests.a2[,4:45]),sparse=T)
+  tests.T.a3<-Matrix(as.matrix(tests.a3[,4:45]),sparse=T)
+  tests.T.a4<-Matrix(as.matrix(tests.a4[,4:45]),sparse=T)
+  tests.T.a5<-Matrix(as.matrix(tests.a5[,4:45]),sparse=T)
+  tests.T.b1<-Matrix(as.matrix(tests.b1[,4:45]),sparse=T)
+  tests.T.b2<-Matrix(as.matrix(tests.b2[,4:45]),sparse=T)
   
   testPredictions.a1 <- predict(object = bst.a1,newdata = tests.T.a1)
   testPredictions.a2 <- predict(object = bst.a2,newdata = tests.T.a2)
@@ -876,10 +1090,6 @@ dlt.clu.xgb.XXIV <- function(data,count,n) {
   ))
     
     
-    
-
-  
-  
   
 }
  
