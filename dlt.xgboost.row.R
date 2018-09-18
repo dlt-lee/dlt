4f511d6da1ab5bbcaadf6bd9dfd0ece2d0499546
dlt-lee <664219378@qq.com>
2018-09-18 12:08:44 +0800

    modify

--- dlt.xgboost.row.R
+++ dlt.xgboost.row.R
@@ -19,47 +19,48 @@ ab.36.row<-dlt.xgboost.XXXVI(data,count,n)
 ab.39.row<-dlt.xgboost.XXXIX(data,count,n)
 ab.42.row<-dlt.xgboost.XXXXII(data,count,n)
 
-i<-1
-ab.result.a1<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-2
-ab.result.a2<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-3
-ab.result.a3<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-4
-ab.result.a4<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-5
-ab.result.a5<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-6
-ab.result.b1<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
-i<-7
-ab.result.b2<-c(tail(ab.1.row,1)[i],tail(ab.2.row,1)[i],tail(ab.3.row,1)[i],tail(ab.6.row,1)[i],
-                tail(ab.9.row,1)[i],tail(ab.12.row,1)[i],tail(ab.15.row,1)[i],tail(ab.18.row,1)[i],
-                tail(ab.21.row,1)[i],tail(ab.24.row,1)[i],tail(ab.27.row,1)[i],tail(ab.30.row,1)[i],
-                tail(ab.33.row,1)[i],tail(ab.36.row,1)[i],tail(ab.39.row,1)[i],tail(ab.42.row,1)[i])
+
+ab.result.a1<-c(tail(ab.1.row$a1,1),tail(ab.2.row$a1,1),tail(ab.3.row$a1,1),tail(ab.6.row$a1,1),
+                tail(ab.9.row$a1,1),tail(ab.12.row$a1,1),tail(ab.15.row$a1,1),tail(ab.18.row$a1,1),
+                tail(ab.21.row$a1,1),tail(ab.24.row$a1,1),tail(ab.27.row$a1,1),tail(ab.30.row$a1,1),
+                tail(ab.33.row$a1,1),tail(ab.36.row$a1,1),tail(ab.39.row$a1,1),tail(ab.42.row$a1,1))
+
+ab.result.a2<-c(tail(ab.1.row$a2,1),tail(ab.2.row$a2,1),tail(ab.3.row$a2,1),tail(ab.6.row$a2,1),
+                tail(ab.9.row$a2,1),tail(ab.12.row$a2,1),tail(ab.15.row$a2,1),tail(ab.18.row$a2,1),
+                tail(ab.21.row$a2,1),tail(ab.24.row$a2,1),tail(ab.27.row$a2,1),tail(ab.30.row$a2,1),
+                tail(ab.33.row$a2,1),tail(ab.36.row$a2,1),tail(ab.39.row$a2,1),tail(ab.42.row$a2,1))
+
+ab.result.a3<-c(tail(ab.1.row$a3,1),tail(ab.2.row$a3,1),tail(ab.3.row$a3,1),tail(ab.6.row$a3,1),
+                tail(ab.9.row$a3,1),tail(ab.12.row$a3,1),tail(ab.15.row$a3,1),tail(ab.18.row$a3,1),
+                tail(ab.21.row$a3,1),tail(ab.24.row$a3,1),tail(ab.27.row$a3,1),tail(ab.30.row$a3,1),
+                tail(ab.33.row$a3,1),tail(ab.36.row$a3,1),tail(ab.39.row$a3,1),tail(ab.42.row$a3,1))
+
+ab.result.a4<-c(tail(ab.1.row$a4,1),tail(ab.2.row$a4,1),tail(ab.3.row$a4,1),tail(ab.6.row$a4,1),
+                tail(ab.9.row$a4,1),tail(ab.12.row$a4,1),tail(ab.15.row$a4,1),tail(ab.18.row$a4,1),
+                tail(ab.21.row$a4,1),tail(ab.24.row$a4,1),tail(ab.27.row$a4,1),tail(ab.30.row$a4,1),
+                tail(ab.33.row$a4,1),tail(ab.36.row$a4,1),tail(ab.39.row$a4,1),tail(ab.42.row$a4,1))
+
+ab.result.a5<-c(tail(ab.1.row$a5,1),tail(ab.2.row$a5,1),tail(ab.3.row$a5,1),tail(ab.6.row$a5,1),
+                tail(ab.9.row$a5,1),tail(ab.12.row$a5,1),tail(ab.15.row$a5,1),tail(ab.18.row$a5,1),
+                tail(ab.21.row$a5,1),tail(ab.24.row$a5,1),tail(ab.27.row$a5,1),tail(ab.30.row$a5,1),
+                tail(ab.33.row$a5,1),tail(ab.36.row$a5,1),tail(ab.39.row$a5,1),tail(ab.42.row$a5,1))
+
+ab.result.b1<-c(tail(ab.1.row$b1,1),tail(ab.2.row$b1,1),tail(ab.3.row$b1,1),tail(ab.6.row$b1,1),
+                tail(ab.9.row$b1,1),tail(ab.12.row$b1,1),tail(ab.15.row$b1,1),tail(ab.18.row$b1,1),
+                tail(ab.21.row$b1,1),tail(ab.24.row$b1,1),tail(ab.27.row$b1,1),tail(ab.30.row$b1,1),
+                tail(ab.33.row$b1,1),tail(ab.36.row$b1,1),tail(ab.39.row$b1,1),tail(ab.42.row$b1,1))
+
+ab.result.b2<-c(tail(ab.1.row$b2,1),tail(ab.2.row$b2,1),tail(ab.3.row$b2,1),tail(ab.6.row$b2,1),
+                tail(ab.9.row$b2,1),tail(ab.12.row$b2,1),tail(ab.15.row$b2,1),tail(ab.18.row$b2,1),
+                tail(ab.21.row$b2,1),tail(ab.24.row$b2,1),tail(ab.27.row$b2,1),tail(ab.30.row$b2,1),
+                tail(ab.33.row$b2,1),tail(ab.36.row$b2,1),tail(ab.39.row$b2,1),tail(ab.42.row$b2,1))
 
 
 
-ab.result<-data.frame(ab.result.a1,ab.result.a2,ab.result.a3,ab.result.a4,ab.result.a5,ab.result.b1,ab.result.b2)
+ab.result<-data.frame(ab.result.a1,ab.result.a2,ab.result.a3,ab.result.a4,
+                      ab.result.a5,ab.result.b1,ab.result.b2)
 
-table(ab.result$a1)
+table(ab.result.a1)
 table(ab.result.a2)
 table(ab.result.a3)
 table(ab.result.a4)
