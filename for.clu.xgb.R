cafe5ad084f718b17c443fe111c7114954eb01ef
dlt-lee <664219378@qq.com>
2018-10-03 18:36:42 +0800

    modify

--- for.clu.xgb.R
+++ for.clu.xgb.R
@@ -18,58 +18,56 @@ ab.39.clu<-dlt.clu.xgb.XXXIX(data,count,n)
 ab.42.clu<-dlt.clu.xgb.XXXXII(data,count,n)
 
 
-ab.result.a1.clu<-c(tail(ab.3.clu$a1,1),tail(ab.6.clu$a1,1),tail(ab.9.clu$a1,1),
+a1.clu<-c(tail(ab.3.clu$a1,1),tail(ab.6.clu$a1,1),tail(ab.9.clu$a1,1),
                     tail(ab.12.clu$a1,1),tail(ab.15.clu$a1,1),tail(ab.18.clu$a1,1),
                     tail(ab.21.clu$a1,1),tail(ab.24.clu$a1,1),tail(ab.27.clu$a1,1),
                     tail(ab.30.clu$a1,1),tail(ab.33.clu$a1,1),tail(ab.36.clu$a1,1),
                     tail(ab.39.clu$a1,1),tail(ab.42.clu$a1,1))
-ab.result.a2.clu<-c(tail(ab.3.clu$a2,1),tail(ab.6.clu$a2,1),tail(ab.9.clu$a2,1),
+a2.clu<-c(tail(ab.3.clu$a2,1),tail(ab.6.clu$a2,1),tail(ab.9.clu$a2,1),
                     tail(ab.12.clu$a2,1),tail(ab.15.clu$a2,1),tail(ab.18.clu$a2,1),
                     tail(ab.21.clu$a2,1),tail(ab.24.clu$a2,1),tail(ab.27.clu$a2,1),
                     tail(ab.30.clu$a2,1),tail(ab.33.clu$a2,1),tail(ab.36.clu$a2,1),
                     tail(ab.39.clu$a2,1),tail(ab.42.clu$a2,1))
-ab.result.a3.clu<-c(tail(ab.3.clu$a3,1),tail(ab.6.clu$a3,1),tail(ab.9.clu$a3,1),
+a3.clu<-c(tail(ab.3.clu$a3,1),tail(ab.6.clu$a3,1),tail(ab.9.clu$a3,1),
                     tail(ab.12.clu$a3,1),tail(ab.15.clu$a3,1),tail(ab.18.clu$a3,1),
                     tail(ab.21.clu$a3,1),tail(ab.24.clu$a3,1),tail(ab.27.clu$a3,1),
                     tail(ab.30.clu$a3,1),tail(ab.33.clu$a3,1),tail(ab.36.clu$a3,1),
                     tail(ab.39.clu$a3,1),tail(ab.42.clu$a3,1))
-ab.result.a4.clu<-c(tail(ab.3.clu$a4,1),tail(ab.6.clu$a4,1),tail(ab.9.clu$a4,1),
+a4.clu<-c(tail(ab.3.clu$a4,1),tail(ab.6.clu$a4,1),tail(ab.9.clu$a4,1),
                     tail(ab.12.clu$a4,1),tail(ab.15.clu$a4,1),tail(ab.18.clu$a4,1),
                     tail(ab.21.clu$a4,1),tail(ab.24.clu$a4,1),tail(ab.27.clu$a4,1),
                     tail(ab.30.clu$a4,1),tail(ab.33.clu$a4,1),tail(ab.36.clu$a4,1),
                     tail(ab.39.clu$a4,1),tail(ab.42.clu$a4,1))
-ab.result.a5.clu<-c(tail(ab.3.clu$a5,1),tail(ab.6.clu$a5,1),tail(ab.9.clu$a5,1),
+a5.clu<-c(tail(ab.3.clu$a5,1),tail(ab.6.clu$a5,1),tail(ab.9.clu$a5,1),
                     tail(ab.12.clu$a5,1),tail(ab.15.clu$a5,1),tail(ab.18.clu$a5,1),
                     tail(ab.21.clu$a5,1),tail(ab.24.clu$a5,1),tail(ab.27.clu$a5,1),
                     tail(ab.30.clu$a5,1),tail(ab.33.clu$a5,1),tail(ab.36.clu$a5,1),
                     tail(ab.39.clu$a5,1),tail(ab.42.clu$a5,1))
-ab.result.b1.clu<-c(tail(ab.3.clu$b1,1),tail(ab.6.clu$b1,1),tail(ab.9.clu$b1,1),
+b1.clu<-c(tail(ab.3.clu$b1,1),tail(ab.6.clu$b1,1),tail(ab.9.clu$b1,1),
                     tail(ab.12.clu$b1,1),tail(ab.15.clu$b1,1),tail(ab.18.clu$b1,1),
                     tail(ab.21.clu$b1,1),tail(ab.24.clu$b1,1),tail(ab.27.clu$b1,1),
                     tail(ab.30.clu$b1,1),tail(ab.33.clu$b1,1),tail(ab.36.clu$b1,1),
                     tail(ab.39.clu$b1,1),tail(ab.42.clu$b1,1))
-ab.result.b2.clu<-c(tail(ab.3.clu$b2,1),tail(ab.6.clu$b2,1),tail(ab.9.clu$b2,1),
+b2.clu<-c(tail(ab.3.clu$b2,1),tail(ab.6.clu$b2,1),tail(ab.9.clu$b2,1),
                     tail(ab.12.clu$b2,1),tail(ab.15.clu$b2,1),tail(ab.18.clu$b2,1),
                     tail(ab.21.clu$b2,1),tail(ab.24.clu$b2,1),tail(ab.27.clu$b2,1),
                     tail(ab.30.clu$b2,1),tail(ab.33.clu$b2,1),tail(ab.36.clu$b2,1),
                     tail(ab.39.clu$b2,1),tail(ab.42.clu$b2,1))
 
 
-sort(table(ab.result.a1.clu))
-sort(table(ab.result.a2.clu))
-sort(table(ab.result.a3.clu))
-sort(table(ab.result.a4.clu))
-sort(table(ab.result.a5.clu))
-sort(table(ab.result.b1.clu))
-sort(table(ab.result.b2.clu))
+ab.result.clu<-data.frame(a1.clu,a2.clu,a3.clu,a4.clu,a5.clu,b1.clu,b2.clu)
 
-ab.result.rclu<-data.frame(ab.result.a1.clu,ab.result.a2.clu,ab.result.a3.clu,
-                           ab.result.a4.clu,ab.result.a5.clu,ab.result.b1.clu,
-                           ab.result.b2.clu)
+sort(table(ab.result.clu$a1.clu))
+sort(table(ab.result.clu$a2.clu))
+sort(table(ab.result.clu$a3.clu))
+sort(table(ab.result.clu$a4.clu))
+sort(table(ab.result.clu$a5.clu))
+sort(table(ab.result.clu$b1.clu))
+sort(table(ab.result.clu$b2.clu))
 
 
 
-ab.result.rclu
+ab.result.clu
 
 
 
