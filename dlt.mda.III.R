6727b97c3a0fd6331dcd36ae286f770da4d0d714
dlt-lee <664219378@qq.com>
2018-10-09 12:56:28 +0800

    modify

--- dlt.mda.III.R
+++ dlt.mda.III.R
@@ -2,7 +2,7 @@ data<-dlt
 count<-dim(dlt)[1]
 data<-dlt
 count<-dim(dlt)[1]
-dlt.mda.III <- function(data,count) {
+#dlt.mda.III <- function(data,count) {
   library(mda)
   library(caret)
   
@@ -2371,6 +2371,109 @@ dlt.mda.III <- function(data,count) {
   testPredictions.b1<-as.numeric(predict(mdaModel.b1,tests.ab))
   testPredictions.b2<-as.numeric(predict(mdaModel.b2,tests.ab))
   
+  ################################################################################
+  rows<-length(testPredictions.a1)-1
+  data.f.ab<-tail(dlt,rows)
+  #A1:
+  a1.Predictions<-head(testPredictions.a1,rows)
+  #a1.Predictions<-floor(a1.Predictions)
+  a1.delta<-data.f.ab$a1-(a1.Predictions)
+  barplot(table(a1.delta),main = "a1")
+  #A2:
+  a2.Predictions<-head(testPredictions.a2,rows)
+  #a2.Predictions<-floor(a2.Predictions)
+  a2.delta<-data.f.ab$a2-a2.Predictions
+  barplot(table(a2.delta),main = "a2") 
+  #A3:
+  a3.Predictions<-head(testPredictions.a3,rows)
+  #a3.Predictions<-floor(a3.Predictions)
+  a3.delta<-data.f.ab$a3-a3.Predictions
+  barplot(table(a3.delta),main = "a3") 
+  #A4:
+  a4.Predictions<-head(testPredictions.a4,rows)
+  #a4.Predictions<-floor(a4.Predictions)
+  a4.delta<-data.f.ab$a4-(a4.Predictions)
+  barplot(table(a4.delta),main = "a4") 
+  #A5:
+  a5.Predictions<-head(testPredictions.a5,rows)
+  #a5.Predictions<-floor(a5.Predictions)
+  a5.delta<-data.f.ab$a5-(a5.Predictions)
+  barplot(table(a5.delta),main = "a5") 
+  #B1:
+  b1.Predictions<-head(testPredictions.b1,rows)
+  #b1.Predictions<-floor(b1.Predictions)
+  b1.delta<-data.f.ab$b1-(b1.Predictions)
+  barplot(table(b1.delta),main = "b1")
+  #B2:
+  b2.Predictions<-head(testPredictions.b2,rows)
+  #b2.Predictions<-floor(b2.Predictions)
+  b2.delta<-data.f.ab$b2-(b2.Predictions)
+  barplot(table(b2.delta),main = "b2")
+  
+  a1.Predictions<-a1.Predictions
+  a2.Predictions<-a2.Predictions+1
+  a3.Predictions<-a3.Predictions+3
+  a4.Predictions<-a4.Predictions+7
+  a5.Predictions<-a5.Predictions+16
+  b1.Predictions<-b1.Predictions
+  b2.Predictions<-b2.Predictions+1
+  
+  ###################################################################################
+  n.a<-0
+  n.b<-0
+  n.c<-0
+  for (i in 1:rows) {
+    temp.a<-0
+    temp.b<-0
+    temp.c<-0
+    if (data.f.ab[i,]$a1==a1.Predictions[i]) {
+      temp.a<-temp.a+1
+    }
+    if (data.f.ab[i,]$a2==a2.Predictions[i]) {
+      temp.a<-temp.a+1
+    }
+    if (data.f.ab[i,]$a3==a3.Predictions[i]) {
+      temp.a<-temp.a+1
+    }
+    if (data.f.ab[i,]$a4==a4.Predictions[i]) {
+      temp.a<-temp.a+1
+    }
+    if (data.f.ab[i,]$a5==a5.Predictions[i]) {
+      temp.a<-temp.a+1
+    }
+    n.a<-c(n.a,temp.a)
+    if (data.f.ab[i,]$b1==b1.Predictions[i]) {
+      temp.b<-temp.b+1
+    }
+    if (data.f.ab[i,]$b2==b2.Predictions[i]) {
+      temp.b<-temp.b+1
+    }
+    n.a<-c(n.a,temp.a)
+    n.b<-c(n.b,temp.b)
+    if (temp.a==5&temp.b==2) {
+      temp.c<-1
+    }
+    else if(temp.a==5&temp.b==1) {
+      temp.c<-2
+    }
+    else if (temp.a==5|(temp.a==4&temp.b==2)) {
+      temp.c<-3
+    }
+    else if ((temp.a==4&temp.b==1)|(temp.a==3&temp.b==2)) {
+      temp.c<-4
+    }
+    else if (temp.a==4|(temp.a==3&temp.b==1)|(temp.a==2&temp.b==2)) {
+      temp.c<-5
+    }
+    else if (temp.a==3|(temp.a==1&temp.b==2)|(temp.a==2&temp.b==1)|temp.b==2) {
+      temp.c<-6
+    }
+    n.c<-c(n.c,temp.c)
+  }
+  table(n.a)
+  table(n.b)
+  table(n.c)
+  
   dlt.p.table(dlt,
               round(testPredictions.a1),round(testPredictions.a2),
               round(testPredictions.a3),round(testPredictions.a4),
@@ -2385,6 +2488,6 @@ dlt.mda.III <- function(data,count) {
           tail(testPredictions.b1,1),
           tail(testPredictions.b2,1)))
   
-}
+#}
 
 
