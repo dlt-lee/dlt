138ce5ebd0d775e051cce4b4990f217fe35c96bf
dlt-lee <664219378@qq.com>
2018-10-19 10:35:45 +0800

    modify

--- dlt.xgb.clu.clu.R
+++ dlt.xgb.clu.clu.R
@@ -1,11 +1,28 @@
-rows<-dim(dlt)[1]-270
+rows<-dim(dlt)[1]-273
+pre.a1<-0
+pre.a2<-0
+pre.a3<-0
+pre.a4<-0
+pre.a5<-0
+pre.b1<-0
+pre.b2<-0
+
+exp.a1<-0
+exp.a2<-0
+exp.a3<-0
+exp.a4<-0
+exp.a5<-0
+exp.b1<-0
+exp.b2<-0
 
 for(i in 1:90) {
   data<-head(dlt,rows)
   data<-tail(data,300)
-  for.clu.xgb(data)
+  temp.data<-for.clu.xgb(data)
+  pre.a1<-c(pre.a1,temp.data$a1)
+  
   rows<-rows+3
-  print("==========================================================")
+  
 }
 
 
