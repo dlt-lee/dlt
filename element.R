3a17769e4057957479d6739083f7fcb9ba638efc
dlt-lee <664219378@qq.com>
2019-01-06 00:05:02 +0800

    modify

--- element.R
+++ element.R
@@ -663,7 +663,8 @@ cell_2018<-c( 18001,01,13,22,25,35,3,04,
               18154,14,22,23,26,30,03,05
               )
 m_2018<-matrix(cell_2018,ncol = 8,byrow = TRUE,dimnames = list(cnames,rnames))
-cell_2019<-c( 19001,06,18,23,26,28,03,12
+cell_2019<-c( 19001,06,18,23,26,28,03,12,
+              19002,02,07,14,22,31,01,07
               )
 m_2019<-matrix(cell_2019,ncol = 8,byrow = TRUE,dimnames = list(cnames,rnames))
 cell_all<-c(cell_2015,
