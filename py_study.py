dfd36c6a60c4a030f28e56d091fed8efd612878f
dlt-lee <664219378@qq.com>
2019-05-18 19:58:51 +0800

    modify

--- py_study.py
+++ py_study.py
@@ -1,1 +1,19 @@
 print(100+200+300)
+print('hello,	world	and	hello	deep	learning!')
+print(type(3))
+print(type('class'))
+print(int(1.23e9))
+print(1.2e-2)
+s = 'abc'
+print(s)
+print(len(s))
+print('I\'m \"OK\"')
+a = 'hello'
+b = 'world'
+c = a + ' like ' + b
+print(c)
+d = 'a: {}, b: {}'.format(23, 34)
+print(d)
+s = 'hello'
+print(s.capitalize())
+print(s.upper())
