02a2359b97861110e301f6824b2714c60341b0ae
dlt-lee <664219378@qq.com>
2019-06-27 15:31:23 +0800

    modify

--- torch_adam.py
+++ torch_adam.py
@@ -1,33 +1,30 @@
-from __future__ import print_function
+import numpy as np
 import torch
 import data_dlt
-import numpy as np
-from torch import nn
 from torch.autograd import Variable
 import time
 import matplotlib.pyplot as plt
-
-#print(data_dlt.get_dim()[0])
-#print(data_dlt.get_trainsX(500).reshape((-1,)))
-#print(data_dlt.get_trainsY(688))
+from torch import nn
 
 def data_tf(x):
-    x = x / 35
+    x = np.array(x, dtype='float32') / 35
     x = (x - 0.5) / 0.5
     x = x.reshape((-1,))
     x = torch.from_numpy(x)
     return x
 
 net = nn.Sequential(
-    nn.Linear(2100, 200),
-    nn.ReLU(),
-    nn.Linear(200, 20),
-    nn.ReLU(),
-    nn.Linear(20, 7),
-    )
+nn.Linear(2100, 1000),
+nn.ReLU(),
+nn.Linear(1000, 500),
+nn.ReLU(),
+nn.Linear(500, 250),
+nn.ReLU(),
+nn.Linear(250, 10),
+)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+optimizer = torch.optim.Adam(net.parameters(),lr=1e-3)
 
 #Training
 line = data_dlt.get_dim()[0]-304
@@ -35,25 +32,13 @@ start = time.time()
 for e in range(100):
     train_loss = 0
     line = line+3
-    #print(line)
-    im = Variable(data_tf(data_dlt.get_trainsX(line)),requires_grad=True)
-    label = Variable(data_tf(data_dlt.get_trainsY(line)),requires_grad=True)
-    #print(data_tf(data_dlt.get_trainsX(line)))
-    #forward
-    print(im.shape)
-    #out = net(im)
-    #print(out)
+    im = Variable(data_tf(data_dlt.get_trainsX(line)), requires_grad=True)
+    label  = Variable(torch.from_numpy(data_dlt.get_trainsY(line)).cuda(0))
+    out = net(im)
+    print(out)
+    #print(label.shape)
     print(label)
-    print('---------------------------------------------------')
+    
+    print('--------------------------------------------------')
     #loss = criterion(out,label)
-    #backward
-    #optimizer.zero_grad()
-    #loss.backward()
-    #optimizer.step()
 
-    #train_loss += loss.data[0]
-
-end = time.time()
-print(data_dlt.get_trainsX(line).shape)
-print(label)
-print (end-start)
--- torch_temp.py
+++ torch_temp.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 
 def data_tf(x):
     x = np.array(x, dtype='float32') / 255
-    x = (x - 0.5) / 0.5 #
-    x = x.reshape((-1,)) #
+    x = (x - 0.5) / 0.5
+    x = x.reshape((-1,))
     x = torch.from_numpy(x)
     return x
 
@@ -19,7 +19,7 @@ train_set = MNIST('./data', train=True, transform=data_tf, download=True)
 test_set = MNIST('./data', train=False, transform=data_tf, download=True)
 
 criterion = nn.CrossEntropyLoss()
-train_data = DataLoader(train_set, batch_size=64, shuffle=True)
+train_data = DataLoader(train_set, batch_size=1, shuffle=True)
 
 net = nn.Sequential(
     nn.Linear(784, 200),
@@ -30,20 +30,28 @@ net = nn.Sequential(
 optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
 
 start = time.time()
-print(test_set)
+#print(type(train_set))
 for e in range(2):
     train_loss = 0
     for im, label in train_data:
         im = Variable(im)
-        print(im.shape)
+        #print(type(im))
+        #print(im.shape)
         label = Variable(label)
-        print(label.shape)
-        print('--------------------------------------------------')
+        #print(type(label))
+        #print(label.shape)
+        
         out = net(im)
+        print(out)
+        #print(type(out))
+        #print(out.shape)
+        print(label)
         loss = criterion(out, label)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        print('--------------------------------------------------')
         #train_loss +=loss
     #print('epoch: {}, Train Loss: {:.6f}'.format(e, train_loss / len(train_data)))
 end = time.time()
