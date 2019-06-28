77536828b49888e719f68fe516a09dcdb82aa8ff
dlt-lee <664219378@qq.com>
2019-06-28 14:32:34 +0800

    modify

--- torch_adam.py
+++ torch_adam.py
@@ -11,38 +11,49 @@ def data_tf(x):
     x = np.array(x, dtype='float32') / 35
     x = (x - 0.5) / 0.5
     x = x.reshape((-1,))
-    x = torch.from_numpy(x)
+    #x = torch.from_numpy(x)
     return x
 
 net = nn.Sequential(
 nn.Linear(2100, 1000),
 nn.ReLU(),
-nn.Linear(1000, 500),
+nn.Linear(1000, 600),
 nn.ReLU(),
-nn.Linear(500, 200),
+nn.Linear(600, 300),
 nn.ReLU(),
-nn.Linear(200, 10),
+nn.Linear(300, 100),
+nn.ReLU(),
+nn.Linear(100, 10),
 )
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(),lr=1e-3)
 
-#Training
+#Build data
+#print(data_dlt.get_dim())
+train_data = np.array ([0])
 line = data_dlt.get_dim()[0]-304
-start = time.time()
-for e in range(100):
-    train_loss = 0
+train_data = data_tf(data_dlt.get_trainsX(line))
+train_data = np.hstack((train_data,data_dlt.get_trainsY(line)))
+for i in range(100):
     line = line+3
-    im = Variable(data_tf(data_dlt.get_trainsX(line)), requires_grad=True)
-    #label  = Variable(torch.from_numpy(data_dlt.get_trainsY(line)).cuda(0))
-    label  = Variable(torch.from_numpy(data_dlt.get_trainsY(line)))
-    #print(data_dlt.get_trainsX(line))
-    print(im)
+    temp_data = data_tf(data_dlt.get_trainsX(line))
+    temp_data = np.hstack((temp_data,data_dlt.get_trainsY(line)))
+    train_data = np.vstack((train_data,temp_data))
+#print(train_data.shape)
+#print(train_data[200,:210])
+#print(train_data[200,210:])
+line = train_data.shape[0]
+#print(line)
+for j in range(0,line):
+    im = Variable(torch.from_numpy(np.array(train_data[j:j+1,:2100],dtype='float32')), requires_grad=True)
+    label = Variable(torch.from_numpy(np.array(train_data[j,2100:2101],dtype='int64')))
+    print(im.shape)
+    
+
     out = net(im)
-    print(out.shape)
-    #print(label.shape)
+    print(out)
     print(label)
-    print('--------------------------------------------------')
+    print('-----------------------------------------------------')
 
-    #loss = criterion(out, label)
-
+    loss = criterion(out, label)
