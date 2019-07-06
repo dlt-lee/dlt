c089b6e7c7319aef389750cd62c905ddcca206f2
dlt-lee <664219378@qq.com>
2019-07-06 22:52:47 +0800

    modify

---
+++ .vscode/helloworld.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+     cout << "Helloworld" << endl;
+
+     int greenMochiInteger = 3;
+     cout << "I have" << greenMochiInteger << " green mochis" << endl;
+
+     double greenMochiDouble = 0.5;
+     cout << "I ate" << greenMochiDouble << " of them." << endl;
+
+     int res = 0;
+     res = greenMochiInteger - greenMochiDouble
+                                   
+     cout<< "I have" << res << " green mochis" << endl;
+}
