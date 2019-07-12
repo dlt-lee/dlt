19c4b073974649c41f69bdcdba77db82d108f212
dlt-lee <664219378@qq.com>
2019-07-12 20:43:49 +0800

    modify

--- torch_rnn.py
+++ torch_rnn.py
@@ -1,20 +1,5 @@
-import torch
-import data_dlt
 from torch import nn
-from torch.autograd import Variable
-
-embeds = nn.Embedding(30, 7)
-embeds.weight
-
-line = data_dlt.get_dim()[0]
-#print(line)
-#print(data_dlt.get_trainsY(line))
-print(data_dlt.get_trainsX(line).shape)
-print(type(data_dlt.get_trainsX(line)))
-
-embeds.weight.data = torch.from_numpy(data_dlt.get_trainsX(line))
-print(embeds.weight)
-
-CONTEXT_SIZE = 7
-EMBEDDING_DIM = 30
+import torch
+import numpy as np
+import data_dlt
 
