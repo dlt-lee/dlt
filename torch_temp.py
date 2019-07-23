951c57d9a92a8569c329bf2a8c3b17726e00549c
dlt-lee <664219378@qq.com>
2019-07-23 23:10:39 +0800

    modify

--- torch_temp.py
+++ torch_temp.py
@@ -1,1 +1,49 @@
 import numpy as np
+import torch
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+from torch import nn
+from torch.autograd import Variable
+import time
+import matplotlib.pyplot as plt
+
+def data_tf(x):
+    x = np.array(x, dtype='float32') / 255
+    x = (x - 0.5) / 0.5 
+    x = x.reshape((-1,)) 
+    x = torch.from_numpy(x)
+    return x
+
+train_set = MNIST('./data', train=True,transform=data_tf,download=True)
+test_set = MNIST('./data', train=False,transform=data_tf,download=True)
+#print(train_set)
+#print(type(train_set))
+
+criterion = nn.CrossEntropyLoss()
+
+train_data = DataLoader(train_set, batch_size=1, shuffle=True)
+#print(train_data)
+#print(type(train_data))
+
+net = nn.Sequential(
+    nn.Linear(784, 200),
+    nn.ReLU(),
+    nn.Linear(200, 10),
+)
+
+sqrs = []
+vs = []
+for param in net.parameters():
+    sqrs.append(torch.zeros_like(param.data))
+    vs.append(torch.zeros_like(param.data))
+t = 1
+losses = []
+idx = 0
+
+start = time.time()
+for im, label in train_data:
+    print(im)
+    print(label)
+    print(im.shape)
+    print(label.shape)
+    print("-----------------------------------------------------------------------------")
