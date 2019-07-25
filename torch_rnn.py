99ce785d7139e6af232bbe02c6060595a9b8d381
dlt-lee <664219378@qq.com>
2019-07-25 14:22:13 +0800

    modify

--- torch_rnn.py
+++ torch_rnn.py
@@ -4,5 +4,9 @@ import numpy as np
 import data_dlt as dlt
 
 rows = dlt.get_dim()[0]
-
 print(rows)
+
+line=rows
+print(dlt.get_trainsX(line))
+print(dlt.get_trainsX(line).shape)
+print(dlt.get_trainsY(line))
