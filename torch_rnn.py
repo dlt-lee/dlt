94443f194b7fad30c98766afad0ab526f3e2c215
dlt-lee <664219378@qq.com>
2019-07-29 13:40:54 +0800

    modify

--- torch_rnn.py
+++ torch_rnn.py
@@ -1,8 +1,11 @@
-from torch import nn
-import torch
 import numpy as np
-import data_dlt as dlt
 import math
+import data_dlt as dlt
+from torch import nn
+import matplotlib.pyplot as plt
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
 
 rows = dlt.get_dim()[0]-2  #move to last result
 print(rows)
@@ -17,5 +20,5 @@ for i in range(math.floor(n)) :
     line = line+3
     train_x = dlt.get_trainsX(line)
     train_y = dlt.get_trainsY(line)
-    print(line)
+    #print(line)
     
