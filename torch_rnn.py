aa791c319ac93ccd3d6d4ae5626d038845a0f474
dlt-lee <664219378@qq.com>
2019-07-29 13:33:36 +0800

    modify

--- torch_rnn.py
+++ torch_rnn.py
@@ -2,21 +2,20 @@ from torch import nn
 import torch
 import numpy as np
 import data_dlt as dlt
+import math
 
-rows = dlt.get_dim()[0]
-#print(rows)
+rows = dlt.get_dim()[0]-2  #move to last result
+print(rows)
 
-line=rows-3
-#print(dlt.get_trainsX(line))
-#print(dlt.get_trainsX(line).shape)
-#print(dlt.get_trainsY(line))
-train_x = dlt.get_trainsX(line)
-train_y = dlt.get_trainsY(line)
-for i in range(200) :
-    line = line-3
-    temp_data_x = dlt.get_trainsX(line)
-    temp_data_y = dlt.get_trainsY(line)
-    train_x = np.concatenate((train_x,temp_data_x),axis=1)
-    train_y = np.concatenate((train_y,temp_data_y),axis=1)
-    print(train_x.shape)
-    print(temp_data_y.shape)
+line = rows%3
+n = (rows-line)/3-10
+
+line = math.floor(rows-n*3)
+print(line)
+
+for i in range(math.floor(n)) :
+    line = line+3
+    train_x = dlt.get_trainsX(line)
+    train_y = dlt.get_trainsY(line)
+    print(line)
+    
