b72e5bc898825eed7c43b74904a2cf58207b22b9
dlt-lee <664219378@qq.com>
2019-08-15 15:30:08 +0800

    modify

--- dlt_sum_L3.R
+++ dlt_sum_L3.R
@@ -1,4 +1,5 @@
 library(HMM)
+library(depmixS4)
 
 #rows<-dim(dlt)[1]
 #line<-rows-98
@@ -119,19 +120,8 @@ ab_m<-matrix(cell,ncol = 8,byrow = TRUE)
 
 dlt_sum_L2(tail(dlt,609))
 
-#构建HMM回归模型
-states<-c("s1","s2","s3","s4","s5")
-numstates<-length(states)
-numstates<-length(states)
-symbols<- c(01,02,03,04,05,06,07,08,09,10,
-            11,12,13,14,15,16,17,18,19,20,
-            21,22,23,24,25,26,27,28,29,30,
-            31,32,33,34,35)
-numsymbols<-length(symbols)
 
-emissionProbilities<-matrix(runif(numstates*numsymbols),numstates,numsymbols)
-emissionProbilities<-sweep(emissionProbilities,1,
-                           rowSums(emissionProbilities),FUN = "/")
 
-hmm<-initHMM(states,symbols,startProbs = startingProbabilities,
-             emissionProbs = emissionProbilities)
+
+
+
