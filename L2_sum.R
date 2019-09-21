b93ed31a3cc6e3c6e504d18450771468e4207bcd
dlt-lee <664219378@qq.com>
2019-09-21 08:25:31 +0800

    add & modify

---
+++ L2_sum.R
@@ -0,0 +1,13 @@
+L2_159<-dlt_sum_cum(tail(dlt,720),159)
+L2_309<-dlt_sum_cum(tail(dlt,720),309)
+L2_459<-dlt_sum_cum(tail(dlt,720),459)
+L2_609<-dlt_sum_cum(tail(dlt,720),609)
+
+
+L2_159
+L2_309
+L2_459
+L2_609
+
+
+.
