fba92e1b62f6fb2d34bcb3e52d43f691d80b9a12
dlt-lee <664219378@qq.com>
2019-10-15 09:16:09 +0800

    modify

--- db_check.R
+++ db_check.R
@@ -15,51 +15,51 @@ library(data.table)
 library(foreach)
 library("doParallel")
 
-med_meeting<-dbConnect(MySQL(),host='rm-2zet5lw17as40fty28o.mysql.rds.aliyuncs.com',port=3306,
+#connect ot Mysql 
+DB_mysql<-dbConnect(MySQL(),host='rm-2zet5lw17as40fty28o.mysql.rds.aliyuncs.com',port=3306,
                       dbname='medo_master',user='snowball',password= 'MEDOsnow$%^&')
 
+#List table in MysQL
+dbListTables(DB_mysql)
 
-dbListTables(med_meeting)
+#Setquery to support chinese
+dbSendQuery(DB_mysql,'SET NAMES gbk')
 
-dbSendQuery(med_meeting,'SET NAMES gbk')
+#Read data from DB and save as data.frame
+mdm_index<-as.data.frame(dbReadTable(DB_mysql,"medmeeting_index"))
+mdm_raw<-as.data.frame(dbReadTable(DB_mysql,"medmeeting_raw"))
+mdm_datil<-as.data.frame(dbReadTable(DB_mysql,"medmeeting_detail"))
 
-rt_index<-as.data.frame(dbReadTable(med_meeting,"medmeeting_index"))
-rt_raw<-as.data.frame(dbReadTable(med_meeting,"medmeeting_raw"))
-rt_datil<-as.data.frame(dbReadTable(med_meeting,"medmeeting_detail"))
-
-write.csv(rt_index,file = "rt_index.csv")
-write.csv(rt_raw,file = "rt_raw.csv")
-write.csv(rt_datil,file = "rt_datil.csv")
-
+#Save data as local file
+write.csv(mdm_index,file = "mdm_index.csv")
+write.csv(mdm_raw,file = "mdm_raw.csv")
+write.csv(mdm_datil,file = "mdm_datil.csv")
+#########################################################################################
+#disconnect DB
+dbDisconnect(med_meeting)
+#########################################################################################
+#Query data by sql
 dbGetQuery(med_meeting,"select Event_ID from medmeeting_index")
-
-Event_ID_detail<-names(table(rt_datil$Event_ID))
-Event_ID__index<-rt_index$Event_ID
-
+########################################################################################
+# case 
+# check leak event_id from mdm_datil to mdm_index
 temp_id<-0
-for ( id in rt_datil$Event_ID) {
-  if (length(rt_index[which(rt_index$Event_ID==id),]$title)>0) {
-    
-  }
+for ( id in mdm_datil$Event_ID) {
+  if (length(mdm_index[which(mdm_index$Event_ID==id),]$title)>0) {}
   else {
     temp_id<-c(temp_id,id)
   }
 }
 temp_id<-temp_id[-1]
 names(table(temp_id))
+########################################################################################
 
-
-dbDisconnect(med_meeting)
 
 
 write.csv(table(rt_datil$session_topic),"t_sess.csv",row.names=FALSE)
 
 event_title<-rt_datil[which(rt_datil$name=="-"),]$event_title
 
-
-foreach(event_title) {
-  print("line\n")
-}
 
 
 
