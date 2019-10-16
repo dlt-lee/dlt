eaa697466c617b39d2117e2d803ad4537de227c7
dlt-lee <664219378@qq.com>
2019-10-16 15:11:35 +0800

    ad file

---
+++ med_n_D.R
@@ -0,0 +1,16 @@
+detail_name<-mdm_datil[which(mdm_datil$DID==""),]$name
+table_name<-as.data.frame(table(detail_name))
+single_name<-table_name[which(table_name$Freq>1),]$detail_name
+single_name<-single_name[-1]
+
+temp_line<-matrix(0,1,16)
+for (nam in single_name) {
+  #print(nam)
+  #print(mdm_datil[which(mdm_datil$name==nam),])
+  dup_DID<-as.matrix(table(mdm_datil[which(mdm_datil$name==nam),]$DID))
+  if (dim(dup_DID)[1]>=2) {
+    temp_line<-rbind(temp_line,as.matrix(mdm_datil[which(mdm_datil$name==nam),])) 
+  }
+  
+}
+write.csv(temp_line,"med_n_D.csv")
